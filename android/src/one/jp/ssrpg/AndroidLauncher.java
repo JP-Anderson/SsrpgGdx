3e4228ba71deb2bdf5944323464b0ab767d4956c
John-Paul Anderson <dahui58@hotmail.com>
2017-04-17 12:50:44 +0100

    Removed unused default Gdx project game class

--- android/src/one/jp/ssrpg/AndroidLauncher.java
+++ android/src/one/jp/ssrpg/AndroidLauncher.java
@@ -4,13 +4,12 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import one.jp.ssrpg.SsrpgGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new SsrpgGdxGame(), config);
+		initialize(new GdxGame(), config);
 	}
 }
--- desktop/src/one/jp/ssrpg/desktop/DesktopLauncher.java
+++ desktop/src/one/jp/ssrpg/desktop/DesktopLauncher.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import one.jp.ssrpg.GdxGame;
-import one.jp.ssrpg.SsrpgGdxGame;
 
 public class DesktopLauncher {
 
